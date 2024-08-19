154071b1e69fd41bac770324778a9073d8db5e35
vijayamadiricde <vijayingcp9066@gmail.com>
2024-08-19 13:29:06 +0530

    Added Lab_03

--- Src/exe_08_18_2024/Lab_02.py
+++ Src/exe_08_18_2024/Lab_02.py
@@ -1,3 +1,5 @@
 print("Hello Vijaya M")
-print("vijay","Ajay","KING",1289756,8867897899, sep=" |",end="\n")
+print("vijay", "Ajay", "KING", 1289756, 8867897899, sep=" |", end="\n")
 print("HANUMAN")
+
+print(max(10,90,400,8.5))
---
+++ Src/exe_08_18_2024/Lab_03.py
@@ -0,0 +1,11 @@
+import keyword
+print(keyword.kwlist)
+
+age = 50
+print(type(age))
+
+age ="vijji"
+print(type(age))
+
+a,b,c = 900, 80, 40
+print(a,b,c,sep="  ")
